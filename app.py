5b1f79d1b5c3fb5692f644ae793302439817e973
Ramanjit Singh <ramanjit.singh@sifars.com>
2024-01-29 12:59:50 +0530

    app.py changed

--- app.py
+++ app.py
@@ -2,7 +2,10 @@
 import streamlit as st
 from langchain_community.callbacks import StreamlitCallbackHandler
 from main import get_agent_executor
-from openai import APIConnectionError
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # CACHE CLEANER
 import atexit, os
@@ -17,14 +20,25 @@ st.set_page_config(page_title="professor's assignment", page_icon="assets/chat.p
 st.title("🦜+ 📰 : LANGCHAIN CHATBOT")
 
 # get api keys and then initialize the agent_executor
-with st.sidebar:
-    api_key=st.text_input("API KEY")
-    azure_endpoint=st.text_input("AZURE ENDPOINT")
+def process_uploaded_file(uploaded_file):
+    content = uploaded_file.read().decode("utf-8")
+    with open(".env", "w") as env_file:
+        env_file.write(content)
+    st.success("File uploaded successfully.")
 
-if not(api_key and azure_endpoint):
-    st.info("Please Enter AZURE API KEY and AZURE ENDPOINT to continue.")
+dot_env_file = st.sidebar.file_uploader(
+    label="Upload .env file containing AZURE API and AZURE ENDPOINT",
+    type=['.txt']
+)
+
+if not dot_env_file:
+    st.info("Please upload .env file to continue.")
     st.stop()
+else:
+    process_uploaded_file(dot_env_file)
 
+api_key = os.getenv("AZURE_OPENAI_API_KEY")
+azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
 agent_executor = get_agent_executor(api_key=api_key, azure_endpoint=azure_endpoint)
 
 # Initialize chat history
@@ -49,12 +63,12 @@ if prompt:
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-with st.spinner():
-    try:
+try:
+    with st.spinner():
         response = agent_executor.invoke({"input":prompt}, callbacks=[StreamlitCallbackHandler])
-    except:
-        st.error("Please check you API KEY and AZURE ENDPOINT")
-        st.stop()
+except:
+    st.error("Please check you API KEY and AZURE ENDPOINT")
+    st.stop()
 # Display assistant response in chat message container
 with st.chat_message("assistant"):
     st.markdown(response["output"])
